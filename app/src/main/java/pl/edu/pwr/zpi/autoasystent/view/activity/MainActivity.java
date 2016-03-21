02663eb9b5f46908eb58a93cab5285a1570d35fe
simur407 <szymon.bartczak@op.pl>
2016-03-21 16:40:38 +0100

    Małe zmiany

--- app/src/main/java/pl/edu/pwr/zpi/autoasystent/view/activity/MainActivity.java
+++ app/src/main/java/pl/edu/pwr/zpi/autoasystent/view/activity/MainActivity.java
@@ -11,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Button button = (Button) findViewById(R.id.color_picker_id);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ColorPickerDialogBuilder
+//                        .with(MainActivity.this)
+//                        .setTitle("Wybierz kolor")
+//                        .wheelType(ColorPickerView.WHEEL_TYPE.CIRCLE)
+//                        .density(12)
+//                        .setOnColorSelectedListener(new OnColorSelectedListener() {
+//                            @Override
+//                            public void onColorSelected(int i) {
+//                                Logger.debug("Color: ", Integer.toHexString(i));
+//                            }
+//                        })
+//                        .setPositiveButton("OK", new ColorPickerClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i, Integer[] integers) {
+//                                Toast.makeText(MainActivity.this, "Color: " + Integer.toHexString(i), Toast.LENGTH_SHORT).show();
+//                            }
+//                        })
+//                        .build()
+//                        .show();
+//            }
+//        });
     }
 }
