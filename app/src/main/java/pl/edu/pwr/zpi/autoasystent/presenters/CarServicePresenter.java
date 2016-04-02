0494bf09635dcb1b0a74c17d6e058c490c12f204
simur407 <szymon.bartczak@op.pl>
2016-04-02 20:24:21 +0200

    Poprawki czesci layoutów i prezenter oraz interfejs serwisów

---
+++ app/src/main/java/pl/edu/pwr/zpi/autoasystent/presenters/CarServicePresenter.java
@@ -0,0 +1,22 @@
+package pl.edu.pwr.zpi.autoasystent.presenters;
+
+import pl.edu.pwr.zpi.autoasystent.model.CarService;
+import pl.edu.pwr.zpi.autoasystent.view.CarServicePanel;
+
+/**
+ * Created by Szymon on 2016-04-02.
+ */
+public class CarServicePresenter {
+
+    CarServicePanel panel;
+
+    public CarServicePresenter(CarServicePanel panel) {
+        this.panel = panel;
+    }
+
+    void saveService(CarService service) {
+        //TODO Save to database
+    }
+
+
+}
