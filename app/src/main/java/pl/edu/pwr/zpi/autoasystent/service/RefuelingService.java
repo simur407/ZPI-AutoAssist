cf15cc166b9d87cd40fd38e4bfb0935e6d25b765
simur407 <szymon.bartczak@op.pl>
2016-04-11 17:34:02 +0200

    Poprawki czesci layoutów i prezenter oraz interfejs serwisów

---
+++ app/src/main/java/pl/edu/pwr/zpi/autoasystent/service/RefuelingService.java
@@ -0,0 +1,49 @@
+package pl.edu.pwr.zpi.autoasystent.service;
+
+import java.util.Date;
+import java.util.List;
+
+import pl.edu.pwr.zpi.autoasystent.model.Refueling;
+
+/**
+ * TODO Dokumentacja
+ *
+ * @author Szymon Bartczak
+ * @date 2016-04-11
+ */
+public class RefuelingService {
+
+    private static RefuelingService instance;
+
+    public static RefuelingService getInstance() {
+        if(instance != null) {
+            instance = new RefuelingService();
+        }
+
+        return instance;
+    }
+
+    private RefuelingService() {
+    }
+
+    public void addRefueling(Refueling refueling) {
+        Refueling.save(refueling);
+    }
+
+    public Refueling findRefuelingById(long id) {
+        return Refueling.findById(Refueling.class, id);
+    }
+
+    public void deleteRefueling(Refueling refueling) {
+        Refueling.delete(refueling);
+    }
+
+    public List<Refueling> getAllRefuelings() {
+        return Refueling.listAll(Refueling.class);
+    }
+
+    public List<Refueling> getRefuelingsByDate(Date from, Date to) {
+        return Refueling.find(Refueling.class, "refueling_date BETWEEN ? AND ?",
+                String.valueOf(from.getTime()), String.valueOf(to.getTime()));
+    }
+}
